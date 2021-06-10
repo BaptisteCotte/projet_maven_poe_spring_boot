81b8aa7cd218e819540c98bf4926a81b18da4219
BaptisteCotte <baptiste.cotte.17@gmail.com>
2021-06-10 16:48:49 +0200

    first

---
+++ src/main/java/fr/formation/projet_maven_poe_spring_boot/ProjetMavenPoeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.projet_maven_poe_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMavenPoeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMavenPoeSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/formation/projet_maven_poe_spring_boot/ProjetMavenPoeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.projet_maven_poe_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetMavenPoeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
